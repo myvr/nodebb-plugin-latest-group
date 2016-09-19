21b2092d36eed31860f44ad8968b06d233382cb3
Danny Eiden <deiden@myvr.com>
2016-09-19 09:54:49 -0700

    Add "don't change group title when group" setting
    Add a setting that allows an admin to select group titles that won't be
    automatically changed by the plugin. In other words, add "sticky" group
    titles.

--- library.js
+++ library.js
@@ -2,16 +2,82 @@
     "use strict";
 
     var async = module.parent.require('async'),
+        db = module.parent.require('./database'),
+        meta = module.parent.require('./meta'),
         user = module.parent.require('./user'),
-        posts = module.parent.require('./posts');
+        groups = module.parent.require('./groups');
 
     var plugin = {};
+
+    plugin.init = function(params, callback) {
+        var app = params.router,
+            middleware = params.middleware;
+
+        app.get('/admin/plugins/latest-group', middleware.admin.buildHeader, renderAdmin);
+        app.get('/api/admin/plugins/latest-group', renderAdmin);
+
+        callback();
+    };
+
+    function renderAdmin(req, res, next) {
+        async.waterfall([
+            function(next) {
+                db.getSortedSetRevRange('groups:createtime', 0, -1, next);
+            },
+            function(groupNames, next) {
+                groupNames = groupNames.filter(function(name) {
+                    return name.indexOf(':privileges:') === -1 && name !== 'registered-users';
+                });
+
+                groups.getGroupsData(groupNames, next);
+            }
+        ], function(err, groupsData) {
+            if (err) {
+                return next(err);
+            }
+
+            res.render('admin', {
+                groups: groupsData
+            });
+        });
+    }
 
     plugin.updateGroupTitle = function(joinData) {
         var uid = joinData.uid,
             groupName = joinData.groupName;
 
-        user.setUserField(uid, 'groupTitle', groupName, null);
+        async.parallel({
+            settings: function(next) {
+                meta.settings.get('latest-group', next);
+
+            },
+            user: function(next) {
+                user.getUserFields(uid, ['uid', 'groupTitle'], next);
+            },
+        }, function(err, results) {
+            if (err) {
+                return;
+            }
+            var query = 'do not change ' + results.user.groupTitle;
+            if (results.settings[query] !== "on") {
+                user.setUserField(
+                    uid,
+                    'groupTitle',
+                    groupName,
+                    null
+                );
+            }
+        });
+    };
+
+    plugin.addAdminNavigation = function(header, callback) {
+        header.plugins.push({
+            route: '/plugins/latest-group',
+            icon: 'fa-users',
+            name: 'Latest Group'
+        });
+
+        callback(null, header);
     };
 
     module.exports = plugin;
